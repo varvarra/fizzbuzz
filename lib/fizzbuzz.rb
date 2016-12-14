8f1993dcf92eabe1e49340c578eb450cb18ec5e1
kateloschinina <ekaterina.loshchinina@gmail.com>
2016-12-14 18:42:36 +0000

    Functionality is written

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz(number)
-  'fizz'
+  if number % 15 == 0
+    return 'fizzbuzz'
+  elsif number % 5 == 0
+    return 'buzz'
+  elsif number % 3 == 0
+    return 'fizz'
+  else
+    return number
+  end
 end
