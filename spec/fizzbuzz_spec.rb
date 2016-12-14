40721f9a69cfea612cac531d5f7330b159cacaf1
Varvara <shinkarenko.varvara@gmail.com>
2016-12-14 18:46:14 +0000

    Adding more tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,7 +10,16 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "7" when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "fizzbuzz" when passed 45' do
+    expect(fizzbuzz(45)).to eq 'fizzbuzz'
+  end
+  it 'returns "buzz" when passed 50' do
+    expect(fizzbuzz(50)).to eq 'buzz'
   end
 end
